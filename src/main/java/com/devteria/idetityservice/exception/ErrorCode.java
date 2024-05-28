1516654b107f3f5926d2b98be4aac9a9d9b5447f
Duongg <lengocduong003@gmail.com>
2024-05-29 01:07:51 +0700

    compelete 14

--- src/main/java/com/devteria/idetityservice/exception/ErrorCode.java
+++ src/main/java/com/devteria/idetityservice/exception/ErrorCode.java
@@ -7,14 +7,15 @@ import org.springframework.http.HttpStatusCode;
 @Getter
 public enum ErrorCode {
     UNCATEGORIZED_EXCEPTION(9999,"Uncategorized error", HttpStatus.INTERNAL_SERVER_ERROR),
-    KEY_INVALID(1001,"Invalid message key", HttpStatus.BAD_REQUEST),
+    KEY_INVALID(1001,"Invalid message key", HttpStatus.BAD_REQUEST), // la rquest cua ng dung
     USER_EXISTED(1002,"User existed", HttpStatus.BAD_REQUEST),
     USERNAME_INVALID(1003,"Username must be at least 3 characters", HttpStatus.BAD_REQUEST),
     PASSWORD_INVALID(1004, "Password must be at least 8 characters", HttpStatus.BAD_REQUEST),
     USER_NOTFOUND(1005,"User not found", HttpStatus.NOT_FOUND),
     UNAUTHENTICATED(1006,"Unauthenticated", HttpStatus.UNAUTHORIZED),
-    UNAUTHORIZED(1007,"You do not have permission", HttpStatus.FORBIDDEN)
-    ;
+    UNAUTHORIZED(1007,"You do not have permission", HttpStatus.FORBIDDEN),
+    INVALID_DOB(1008,"Invalid date of birth", HttpStatus.BAD_REQUEST)
+            ;
 
     ErrorCode(int code, String message, HttpStatusCode statusCode) {
         this.code = code;
